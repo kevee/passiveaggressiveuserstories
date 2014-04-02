c6a96aa965ee781783e28d34c120fc4886c23f5e
Kevin Miller <kevin@csumb.edu>
2014-04-02 16:46:56 -0700

    More corpus

--- js/passive-aggressive.js
+++ js/passive-aggressive.js
@@ -7,20 +7,34 @@
     profile : [
       'idiot',
       'looser',
-      'jerk'
+      'jerk',
+      'user',
+      'lowly employee',
+      'mailroom gal'
     ],
     action : [
       'I want to have a meaningful interaction with another jerk face',
-      'I want to add a meaningless product to my "virtual cart"',
+      'I want to add a meaningless product to my "virtual cart',
       'I want to waste time tooling around on a website',
-      'I guess I want to log into the site'
+      'I guess I want to log into the site',
+      'I want to be distracted by flashing advertisements',
+      'I feel like clicking something blue',
+      'I guess I\'d like to do something',
+      'I want to be wowed by your design skills',
+      'I want to change the font to Comic Sans'
     ],
     purpose : [
       'because I said so',
       'because my boss is an asshole',
       'so I can get back to everyone who laughed at me in high school',
       'because I am having an existential crisis',
-      'so I can forget how sad my life is'
+      'so I can forget how sad my life is',
+      'because committee',
+      'because Facebook',
+      'because someone said SEO',
+      'so I can file a greivance against my exploitative boss',
+      'because we should have done this 5 years ago',
+      'since I guess I\'m here and stuff'
     ]
   };
 })(this);
